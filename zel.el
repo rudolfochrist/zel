c4ba007249de5adb443adb3a5b4294a1211c240d
Sebastian Christ <rudolfo.christ@gmail.com>
2017-10-07 20:54:25 +0200

    Bump development version v0.2.0-pre

--- zel.el
+++ zel.el
@@ -4,7 +4,7 @@
 
 ;; Author: Sebastian Christ <rudolfo.christ@gmail.com>
 ;; URL: https://github.com/rudolfochrist/zel
-;; Version: 0.1.0
+;; Version: 0.2.0-pre
 ;; Package-Requires: ((emacs "25") cl-lib frecency)
 ;; Keywords: convenience, files, matching
 
